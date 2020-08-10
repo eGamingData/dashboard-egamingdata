bc0a3b245a6c67ed026eb40fcd3fe61283af2358
Xavier <crafttutos@gmail.com>
2020-08-10 23:04:46 +0200

    New home dashboard layout

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,34 +2,117 @@
 defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <?php include viewPath('includes/header'); ?>
+<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-<!-- Content Header (Page header) -->
+
+<script type="text/javascript">
+  twttr.widgets.createTimeline(
+  {
+    sourceType: "profile",
+    screenName: "TwitterDev",
+    theme: "dark",
+    height: "100px"
+  },
+ 
+);
+</script>
+
+<!-- Content Header (Page header)  -->
 <section class="content-header">
   <h1>
     eGaming<span class="purple">Data</span> - Home
     <small>esports data for betting purpouses.</small>
-  </h1>
+  </h1> 
   <span class="pull-right-container">
       
     </span>
-</section>
+</section> 
 
 <!-- Main content -->
 <section class="content">
 
-  <div class="alert alert-info alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h4><i class="icon fa fa-info"></i> Information!</h4>
-           eGamingData is under an early phase of development. Service interruptions may occur from time to time.
+  <div class="row">
+  <div class="col-sm-8"><iframe style=" border-radius: 5px;"width="100%" height="500rem" src="https://www.youtube.com/embed/ScMzIvxBSi4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <div class="col-sm-4 twitter-box">
+    <a class="twitter-timeline"
+  href="https://twitter.com/egamingdata"
+  data-height="50rem">
+Tweets by @eGamingData
+</a>
+    
   </div>
+</div>
+<section class="content-header no-margin-left">
+  <h1>
+    Explore our <span class="purple">data</span> 
+  </h1> 
+  <span class="pull-right-container">
+      
+    </span>
+</section>
   <!-- Default box -->
-  <div class="box">
-    <div class="box-body-nomargin">
-      <embed src="https://www.esportscalendar.com" style="width: 100%; height: 650px;">
+  <div class="row">
+    <div class="col-lg-3 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-aqua lol-box">
+        <div class="inner">
+          <h3>League of Legends</h3>
+          <p>Last data entry:</p>
+        </div>
+        <div class="icon">
+        </div>
+        <a href="#" class="small-box-footer">         
+          <p>Search data <i class="fa fa-arrow-circle-right"></i></p> 
+        </a>
+      </div>
     </div>
-
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-green darts-box">
+        <div class="inner">
+          <h3>Darts<sup style="font-size: 20px"></h3>
+          <p>Last data entry:</p>
+        </div>
+        <div class="icon">
+        </div>
+        <a href="#" class="small-box-footer">         
+          <p>Search data <i class="fa fa-arrow-circle-right"></i></p> 
+        </a>
+      </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-yellow dota-box">
+        <div class="inner">
+          <h3>Dota</h3>
+          <p>Last data entry:</p>
+        </div>
+        <div class="icon">
+        </div>
+        <a href="#" class="small-box-footer">         
+          <p>Search data <i class="fa fa-arrow-circle-right"></i></p> 
+        </a>
+      </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+      <!-- small box -->
+      <div class="small-box bg-red csgo-box">
+        <div class="inner">
+          <h3>CS:GO</h3>
+          <p>Last data entry:</p>
+        </div>
+        <div class="icon">
+        </div>
+        <a href="#" class="small-box-footer">         
+          <p>Search data <i class="fa fa-arrow-circle-right"></i></p> 
+        </a>
+      </div>
+    </div>
+    <!-- ./col -->
   </div>
-
 </section>
 <!-- /.content -->
 
